56cf25141531073b30a5fa783e81f5059a22fda6
kamil <kamilkonfederak@interia.pl>
2023-09-22 19:12:10 +0200

    Fix autoscaling, add barrel tap

--- Assets/Scripts/player/AutoScaler.cs
+++ Assets/Scripts/player/AutoScaler.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AutoScaler : MonoBehaviour
 {
     [SerializeField] private float defaultHeight = 1.0f;
     [SerializeField] private new Camera camera;
-    [SerializeField] private Transform attachPointLeft;
-    [SerializeField] private Transform attachPointRight;
+    [SerializeField] private List<Transform> scaleList = new List<Transform>();
+    [SerializeField] private List<Transform> rescaleList = new List<Transform>();
+    [SerializeField] private Transform rightHandChild;
+    [SerializeField] private GameObject rightHandGameObj;
+    private bool addRightOnce = false;
+
+    private void FindChild() {
+        rightHandChild = rightHandGameObj.transform.Find("[RightHand] Attach");
+        scaleList.Add(rightHandChild);
+    }
 
     private void Resize()
     {
@@ -18,8 +27,15 @@ public class AutoScaler : MonoBehaviour
         }
         float scale = defaultHeight / headHeight;
         transform.localScale = Vector3.one * scale;
-        attachPointLeft.localScale = Vector3.one;
-        attachPointRight.localScale = Vector3.one;
+        foreach (Transform t in scaleList) {
+            if (t == null) continue;
+            t.localScale = Vector3.one / scale;
+        }
+        foreach (Transform t in rescaleList)
+        {
+            if (t == null) continue;
+            t.localScale = Vector3.one * scale;
+        }
     }
 
     private void Start()
@@ -27,9 +43,21 @@ public class AutoScaler : MonoBehaviour
         StartCoroutine(ResizeAfterHeadLoaded());
     }
 
+  /*  private void Update()
+    {
+        if (!addRightOnce) {
+            if (rightHandGameObj != null) //chcialem sprawdzac, kiedy transform bedzie mial childa, ale null to co innego
+            {
+                addRightOnce = true;
+            }
+            //FindChild();
+        }
+    }*/
+
     IEnumerator ResizeAfterHeadLoaded()
     {
         yield return null;
+        //FindChild();
         Resize();
     }
 }
