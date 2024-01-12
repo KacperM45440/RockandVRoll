96b5582596216ff060f8a0c57fe6b192a1ea6b93
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-01-13 00:53:40 +0100

    Hand physics - test 1

--- Assets/Scripts/Player/Hands/HandPhysics.cs
+++ Assets/Scripts/Player/Hands/HandPhysics.cs
@@ -12,6 +12,14 @@ public class HandPhysics : MonoBehaviour
     public Transform colliderTransform;
     private Collider[] handColliders;
 
+    //public float addForceStrength;
+    //public float velocityStrength;
+
+    public float toVel = 2.5f;
+    public float maxVel = 15.0f;
+    public float maxForce = 40.0f;
+    public float gain = 5f;
+
     void Start()
     {
         bodyRef = GetComponent<Rigidbody>();
@@ -30,7 +38,32 @@ public class HandPhysics : MonoBehaviour
         Vector3 currentPosition = transform.position;
         Vector3 newPosition = Vector3.Lerp(currentPosition, targetPosition, 1f);
 
-        bodyRef.velocity = (newPosition - currentPosition) / Time.deltaTime;
+        //bodyRef.velocity = (newPosition - currentPosition) / Time.deltaTime;
+
+        //bodyRef.velocity = Vector3.ClampMagnitude(bodyRef.velocity, velocityStrength);
+
+        Vector3 dist = targetPosition - transform.position;
+        //dist.y = 0; // ignore height differences
+                    // calc a target vel proportional to distance (clamped to maxVel)
+        Vector3 tgtVel = Vector3.ClampMagnitude(toVel * dist, maxVel);
+        // calculate the velocity error
+        Vector3 error = tgtVel - bodyRef.velocity;
+        // calc a force proportional to the error (clamped to maxForce)
+        Vector3 force = Vector3.ClampMagnitude(gain * error, maxForce);
+        bodyRef.AddForce(force);
+
+
+
+        //if (Vector3.Distance(currentPosition, newPosition) < 0.1f)
+        //{
+        //    bodyRef.velocity *= 0.2f;
+        //}
+        //else
+        //{
+        //    bodyRef.AddForce(addForceStrength * Time.deltaTime * (newPosition - currentPosition), ForceMode.Impulse);
+        //}
+        
+        
     }
     public void Delay(float time)
     {
