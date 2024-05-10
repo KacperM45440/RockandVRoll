5e01f88da21594514e287be9d7386efe32bb24d2
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-05-10 18:00:31 +0200

    Scene load touch up

--- Assets/Scripts/SceneManagers/DoorSceneLoader.cs
+++ Assets/Scripts/SceneManagers/DoorSceneLoader.cs
@@ -15,24 +15,18 @@ public class DoorSceneLoader : MonoBehaviour
 
     // public string sceneName;
     [Header("Load Level")]
-    public UnityEngine.Object choosenLevel;
+    public UnityEngine.Object chosenLevel;
     public float fadeInTime = 0.5f;
     public float fadeOutTime = 1f;
     public Collider playerCollider;
 
     private void OnTriggerEnter(Collider other)
     {
-        // przypisac collider gracza i sprawdzic czy collider ktory wszedl to jest ten z referencji /done
-        // player experience przy ³adowaniu przejœcia by³by miserable - portal netherowy jest mêcz¹cy z czasem (a to tylko jeden portal do jednego miejsca, a nie wszystkich miejsc)
-        // no chyba ¿e na czas fade, to to by mia³o jeszcze sens
-       
         if (other.Equals(playerCollider))
         {
             StartCoroutine(LoadIn(false));
         }
     }
-
-
     public IEnumerator LoadIn(bool loadIn)
     {
         bool transitionDone = false;
@@ -44,7 +38,7 @@ public class DoorSceneLoader : MonoBehaviour
         {
             ScreenFader.Instance.FadeOut(fadeOutTime, () => transitionDone = true);
             yield return new WaitUntil(() => transitionDone);
-            SceneManager.LoadScene(choosenLevel.name);
+            SceneManager.LoadScene(chosenLevel.name);
         }
     }
 }
